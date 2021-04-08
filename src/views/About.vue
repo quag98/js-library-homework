37100087a7baa1b306b109b023c45d7af3856143
David Quaggin-Smith <quagginsmith@gmail.com>
2021-04-08 10:53:40 -0500

    adds homework

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is a map</h1>
+    <div id ="map"></div>
   </div>
 </template>
+
+
+<style type="text/css">
+#map {
+  width: 400px;
+  height: 400px;
+}
+</style>
+
+<script>
+/* global GMaps */
+export default {
+  created: function() {
+    this.googleMap();
+  },
+  methods: {
+    googleMap: function() {
+       var map = new GMaps({
+      el: '#map',
+      lat: -12.043333,
+      lng: -77.028333
+    });
+    console.log(map);
+    console.log(GMaps);
+    }
+}
+}
+</script>
